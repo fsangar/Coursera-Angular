3f0c367472a1e41953f71cc240238c873b444b60
fsangar <57873286+fsangar@users.noreply.github.com>
2022-10-06 14:34:14 +0200

    Formularios y validaciones

--- angular-wishlist-v1/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ angular-wishlist-v1/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {DestinoViaje} from "../models/destino-viaje.model";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-form-destino-viaje',
@@ -13,11 +13,18 @@ export class FormDestinoViajeComponent implements OnInit {
   // Definimos el formularo que es de tipo FromGruop
   fg: FormGroup;
   // FormBuilder permite definir la construcción del formulario, nombre y url serán los elementos vinculados al formulario, permitirá hacer validaciones sobre los mismos
+
+  minLongitud = 3;
+
   constructor(fb: FormBuilder) {
     this.onItemAdded = new EventEmitter<DestinoViaje>();
     this.fg = fb.group({
-      // nombre es requerido
-      nombre: ["", Validators.required],
+      // Validators.compose permite multiples validaciones
+      nombre: ["", Validators.compose([
+        Validators.required,
+        this.nombreValidator,
+        this.nombreValidatorParametrizable(this.minLongitud)
+      ])],
       url: [""]
     });
     // Definimos un observable. Permite comprobar el cambio de un elemento, en este caso del formulario definido como fg
@@ -32,5 +39,26 @@ export class FormDestinoViajeComponent implements OnInit {
     let d = new DestinoViaje(nombre,url);
     this.onItemAdded.emit(d);
     return false;
+  }
+
+  // Las validaciones devuelven como resultado clave y valor
+  // Ej. {required: true}
+  nombreValidator(control: FormControl): {[s:string]:boolean}{
+    const l = control.value.toString().trim().length;
+    if (l > 0 && l < 5){
+      return {invalidNombre: true};
+    }
+    return {};
+  }
+
+  // Otra opción de hacer validadores es utilizando valores parametrizables utilizando lambda
+  nombreValidatorParametrizable(minLong: number):ValidatorFn{
+    return (control: AbstractControl): { [key: string]: boolean } | null => {
+      const l = control.value.toString().trim().length;
+      if (l > 0 && minLong < 5){
+        return {minLongNombre: true};
+      }
+      return {};
+    }
   }
 }
