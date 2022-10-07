e2b109b8e86ac471b351cb961b7a81b809ae55bf
fsangar <57873286+fsangar@users.noreply.github.com>
2022-10-07 11:29:37 +0200

    Cambiado elt ipo de datos para la petición ajax

--- angular-wishlist-v1/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ angular-wishlist-v1/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -16,7 +16,7 @@ export class FormDestinoViajeComponent implements OnInit {
   fg: FormGroup;
 
   minLongitud = 3;
-  searchResults= null;
+  searchResults:string[]= [];
 
   // FormBuilder permite definir la construcción del formulario, nombre y url serán los elementos vinculados al formulario, permitirá hacer validaciones sobre los mismos
   constructor(fb: FormBuilder) {
@@ -38,20 +38,26 @@ export class FormDestinoViajeComponent implements OnInit {
   ngOnInit(): void {
     // Capturamos el evento del input con fromEvent
     let elemNombre = <HTMLInputElement>document.querySelector("#nombre");
+    // Utilizamos un selector de eventos, parametros elelemento y nombre del evento.
     fromEvent(elemNombre, 'input')
       // Pipe nos permite hacer secuencias de códigos condicionales a que las anteriores hayan funcionado
       .pipe(
+        // Convierte el evento de teclado 'e' a un tipo elemento de HTML y obtenemos su valor completo.
         map((e)=> (e.target as HTMLInputElement).value),
+        // Buscamos cuando la longitud es mayor de 2
         filter(text => text.length > 2),
-        // Se queda parado 200ms, no permite introducir más de un carácter en < 200ms
+        // Sólo busca si las teclas se han pulsado con diferencia de >= 200ms
         debounceTime(200),
-        // Buscamos datos distintos
+        // Buscamos si ha cambiado el valor del input, descarta si el usuario pone una x y la borra
         distinctUntilChanged(),
         // Mandaría el texto al webService
-        switchMap(() => ajax("assets/datos.json"))
-      ).subscribe((ajaxResponse => {
+        switchMap(() => ajax("assets/datos.json")),
+      )
+      .subscribe(ajaxResponse => {
+        // Almacenamos el resultado de la búsqueda
         console.log(ajaxResponse.response);
-    }));
+        this.searchResults = ajaxResponse.response as string[];
+    });
   }
   // Función que crea un destion con su nombre y url y emite un evento al componente superior.En nuestro caso el componente superior es el que crea este, es decir lista-destino-viajes
   guardar(nombre: string, url:string):boolean {
