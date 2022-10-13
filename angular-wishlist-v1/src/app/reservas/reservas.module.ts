dd86e121c98b3920fef95f2c057d8faaa64d4add
fsangar <57873286+fsangar@users.noreply.github.com>
2022-10-13 10:21:09 +0200

    Añadido un servicio que obtiene datos de viajes

--- angular-wishlist-v1/src/app/reservas/reservas.module.ts
+++ angular-wishlist-v1/src/app/reservas/reservas.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ReservasRoutingModule } from './reservas-routing.module';
 import { ReservasDetalleComponent } from './reservas-detalle/reservas-detalle.component';
 import { ReservasListadoComponent } from './reservas-listado/reservas-listado.component';
+import {ReservasApiClientService} from "./reservas-api-client.service";
 
 
 @NgModule({
   declarations: [
     ReservasDetalleComponent,
-    ReservasListadoComponent
+    ReservasListadoComponent,
   ],
   imports: [
     CommonModule,
     ReservasRoutingModule
+  ],
+  providers: [
+    ReservasApiClientService
   ]
 })
 export class ReservasModule { }
