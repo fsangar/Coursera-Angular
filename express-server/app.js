ab541f2503ea82ee3c530abc0e0f43f5ac196429
fsangar <57873286+fsangar@users.noreply.github.com>
2023-03-07 11:27:55 +0100

    Finalizado curso 2022/2023

--- express-server/app.js
+++ express-server/app.js
@@ -1,7 +1,9 @@
-// creamos una instancia de express y le decimos que va a usar JSON
-var express = require("express");
+// Permite escribir en un fichero
 var fs = require("fs");
+// Soluciona error CORS
 const cors = require('cors');
+// Creamos una instancia de express y le decimos que va a usar JSON
+var express = require("express");
 var app = express();
 app.use(express.json());
 // Evitar CORS
@@ -11,7 +13,7 @@ app.use(cors({
 
 
 // Abrimos el puerto de escucha al 3000 y una vez abierto mostramos un mensaje.
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => console.log("El servidor está escuchando en el puerto 3000"));
 
 // Devolvemos una respuesta sobre una petición GET
 app.get("/url", (req,res,next) =>
@@ -19,10 +21,12 @@ app.get("/url", (req,res,next) =>
 
 // Creamos una variable JSON
 var destinosFichero = "destinos.json";
+// Leemos el listado de destinos almacenados en JSON
 var misDestinos = JSON.parse(fs.readFileSync(destinosFichero));
 
 // Devolvemos una respuesta sobre una petición GET dinámica
 // Parámetros req = request, res = response, next
+// Devuelve la petición sobre IP/ciudades
 app.get("/ciudades", (req,res,next) => {
     res.json(misDestinos.filter((c)=> c.toLowerCase().indexOf(req.query.q.toString().toLowerCase())> -1));
 });
@@ -32,8 +36,8 @@ app.get("/ciudades", (req,res,next) => {
 app.get("/my", (req,res,next) => res.json(misDestinos));
 
 // Almacenamos un valor de una petición POST
-app.post("/my", (req,res,next) => {
-    console.log(req.body);
+app.post("/destinos", (req,res,next) => {
+    // El destino nuevo se introduce en el cuerpo de la petición
     for (const reqElement of req.body) {
         misDestinos.push(reqElement);
     };
@@ -42,10 +46,11 @@ app.post("/my", (req,res,next) => {
 });
 
 // Actualizamos un valor introduciendo su nombre por parámetros
-app.put("/my/:name", (req,res,next) => {
+app.put("/destinos/:name", (req,res,next) => {
     // :name corresponde con req.params.name
     let nameIndex = misDestinos.indexOf(req.params.name);
     if(nameIndex>= 0 && req.body != null){
+        //El parametro a cambiar se introduce en el cuerpo de la petición
         misDestinos[nameIndex] = req.body[0];
     }
     else {
@@ -55,7 +60,7 @@ app.put("/my/:name", (req,res,next) => {
     res.json(misDestinos[nameIndex]);
 });
 // Borramos un valor introduciendo su nombre por parámetros
-app.delete("/my/:name", (req,res,next) => {
+app.delete("/destinos/:name", (req,res,next) => {
     // :name corresponde con req.params.name
     let nameIndex = misDestinos.indexOf(req.params.name);
     if(nameIndex>= 0){
